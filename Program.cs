7c987718177e32ef6f3c5be11840d9b7ca83f1d8
Jimmy Mead <jdmead@asu.edu>
2019-08-12 12:29:51 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Puzzles
+{
+    class Program
+    {
+        // RANDOM ARRAY
+        public static int [] RandomArray()
+        {
+            var rand = new Random();
+            var RandomArray = new int[10];
+            
+        }
+    }
+}
